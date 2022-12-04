35aa6a1ac6f2bfb22489719e0fa22db03d79adac
Ackon Samuel <ackons433@gmail.com>
2022-12-04 19:17:23 +0000

    Create contact model

---
+++ db/migrate/20221204191644_create_contacts.rb
@@ -0,0 +1,10 @@
+class CreateContacts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contacts do |t|
+      t.string :email
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
