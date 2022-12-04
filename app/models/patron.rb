281cb358145e1908bd7aba17454a4db92cf13aff
Ackon Samuel <ackons433@gmail.com>
2022-12-04 19:21:29 +0000

    Create patron model

---
+++ app/models/patron.rb
@@ -0,0 +1,2 @@
+class Patron < ApplicationRecord
+end
---
+++ db/migrate/20221204192042_create_patrons.rb
@@ -0,0 +1,9 @@
+class CreatePatrons < ActiveRecord::Migration[7.0]
+  def change
+    create_table :patrons do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
